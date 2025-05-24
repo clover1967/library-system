cdadce2ceb05be9916a3798e5253137465340824
clover1967 <46044770+clover1967@users.noreply.github.com>
2025-05-24 11:11:32 +0800

    Add files via upload

---
+++ src/main/java/com/library/model/User.java
@@ -0,0 +1,68 @@
+package com.library.model;
+
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<String> roles = new HashSet<>();
+
+    public User() {}
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.roles.add(role);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+
+    public String getRole() {
+        return roles.stream().findFirst().orElse(null);
+    }
+
+    public void setRole(String role) {
+        this.roles.clear();
+        this.roles.add(role);
+    }
+}
